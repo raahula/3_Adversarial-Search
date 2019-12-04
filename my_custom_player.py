fee8258c9a9aec1a66481fcb8e6de5e10a63e90d
Rahul Dixit <raahula.dixit@gmail.com>
2019-12-04 17:47:00 -0500

    refactor

--- my_custom_player.py
+++ my_custom_player.py
@@ -6,6 +6,8 @@ import numpy as np
 from collections import defaultdict
 from abc import ABC, abstractmethod
 
+BUFFER_TIME = .005
+time_limit = .150 - BUFFER_TIME
 
 class CustomPlayer_alpha_beta(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -171,40 +173,90 @@ class CustomPlayer_mcts(DataPlayer):
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
         
+        '''
+        start_time = time.time()
+        if state.ply_count<2:
+            self.queue.put(random.choice(state.actions()))
+        else:        
+            while time.time() - start_time < time_limit:
+                self.queue.put(self.best_action(state))
+        '''
+        #start_time = time.time()
+        
+        #if state.ply_count<2:
+        #    self.queue.put(random.choice(state.actions()))
+        #else:        
+        self.queue.put(self.best_action(state))        
+
+        '''
         if state.ply_count<2:
             self.queue.put(random.choice(state.actions()))
         else:
-            #for d in range (1, 100):
-            self.queue.put(self.best_action(state, 450))
+            for d in range (49, 50):
+                self.queue.put(self.best_action(state))            
 
-    def best_action(self, state, simulation_time):
+            
+            if self.best_action(state, 50):
+                self.queue.put(self.best_action(state, 50))
+            elif state.actions():
+                self.queue.put(random.choice(state.actions()))
+            else:
+                self.queue.put(None)
+            '''
+
+    #def best_action(self, state, simulation_time):
+    def best_action(self, state):
         self.root = monteCarloNode(state, parent=None)
-        start = time.time()
+
+        '''
         max_time = (math.ceil(simulation_time*0.75))
-        time_spent = (time.time()-start) * 1000
-        while time_spent<= max_time:
+        start = time.time()
+       
+        while ((time.time()-start) * 1000) <= max_time:
             v = self._tree_policy()
             reward = v.rollout()
-            v.backpropogate(reward)
-        return self.root.best_child(c_parameter = 0)
+            v.backpropagate(reward)
+        return self.root.best_child(c_parameter = 0.5)
+        v = self._tree_policy()
+        reward = v.rollout()
+        v.backpropagate(reward)
+        '''
+
+        
+
+        #use this for simulation count 
+
+        for _ in range(0, 40):           
+            v = self._tree_policy()
+            reward = v.rollout()
+            v.backpropagate(reward)
+        return self.root.best_child(c_parameter = 0.5)
+
+        
+
+
+        '''
+
+        #use this for time 
+        start_time = time.time()
+
+        while time.time() - start_time < time_limit:           
+            v = self._tree_policy()
+            reward = v.rollout()
+            v.backpropagate(reward)
+        return self.root.best_child(c_parameter = 0.5)
+
+        '''
 
     def _tree_policy(self):
         current_node = self.root
         while not current_node.is_terminal_node():
-            while not current_node.fully_expanded():
+            if not current_node.fully_expanded():
                 #print("tree_policy current node "+str(current_node))
                 return current_node.expand()
-        else:
-            current_node = current_node.best_child()
+            else:
+                current_node = current_node.best_child()
         return current_node
-
-    def score(self, state):
-        own_loc = state.locs[self.player_id]
-        opp_loc = state.locs[1 - self.player_id]
-        own_liberties = state.liberties(own_loc)
-        opp_liberties = state.liberties(opp_loc)
-        return len(own_liberties) - len(opp_liberties)
-
 
 class monteCarloNode():
     def __init__(self, state, parent = None):
@@ -217,26 +269,28 @@ class monteCarloNode():
         self.parent = parent
 
     def fully_expanded(self):
-        return len(self.untried_action()) == 0
+        return len(self.untried_action) == 0
 
     def best_child(self, c_parameter=1.4):
-        weight = [(c.q()/ c.n())+c_parameter*np.sqrt((2*np.log(self.n())/c.n())) for c in self.children]
+        weight = [(c.q/ c.n) + c_parameter*np.sqrt((2*np.log(self.n)/c.n)) for c in self.children]
         return self.children[np.argmax(weight)]
 
     def rollout_policy(self, possible_moves):
         return random.choice(possible_moves)
     
+    @property
     def untried_action(self):
         #print(self.state.actions)
-        if self._untried_action == None:
+        if self._untried_action is None:
             self._untried_action = self.state.actions() #available legal actions
         return self._untried_action
-    	
+    
+    @property
     def q(self):
-        wins = self._result[1-self.parent.state.player()]
-        losses = self._result[self.parent.state.player()]
+        wins = self._result[self.parent.state.player()]
+        losses = self._result[1-self.parent.state.player()]
         return wins - losses
-
+    @property
     def n(self):
         return self._visits
 
@@ -246,7 +300,7 @@ class monteCarloNode():
         #self._untried_action.remove(p)
         #action=None
         #if self.untried_action()!=[]:
-        action = self.untried_action().pop()
+        action = self.untried_action.pop()
         #action = p
         next_state = self.state.result(action)
         child_node = monteCarloNode(next_state, parent = self)
@@ -256,24 +310,25 @@ class monteCarloNode():
     def is_terminal_node(self):
         return self.state.terminal_test()
 
-    def rollout(self):
+    def rollout(self):   #updated
         current_rollout_state = self.state
+        player = self.state.player()
         while not current_rollout_state.terminal_test():
             possible_moves = current_rollout_state.actions()
             action = self.rollout_policy(possible_moves)
             current_rollout_state = current_rollout_state.result(action)
-            if current_rollout_state.utility(1-current_rollout_state.player()) == float("inf"):
-                return 1
-            if current_rollout_state.utility(1-current_rollout_state.player()) == float("-inf"):
-                return -1
-            else:
-                return 0
+            if current_rollout_state.utility(player) == float("inf"):
+                return player
+            if current_rollout_state.utility(player) == float("-inf"):
+                return (1-player)
+            #else:
+                #return 0
 
-    def backpropogate(self, result):
+    def backpropagate(self, result):
         self._visits+=1
         self._result[result] += 1
         if self.parent:
-            self.parent.backpropogate(result)
+            self.parent.backpropagate(result)
 
 CustomPlayer = CustomPlayer_mcts
 
@@ -287,7 +342,7 @@ class monteCarloTreeSearch(object):
 		for _ in range(simulation_number):
 			v = self._tree_policy()
 			reward = v.rollout()
-			v.backpropogate(reward)
+			v.backpropagate(reward)
 		return self.root.best_child(c_parameter = 0)
 
 	def _tree_policy(self):
