877aa9f119056c7f2b36de3028d167ccb4c32a2c
Rahul Dixit <raahula.dixit@gmail.com>
2019-12-10 22:45:30 -0800

    Bug: get_action-self.root.state.actions()

--- my_custom_player.py
+++ my_custom_player.py
@@ -6,11 +6,133 @@ import time
 from collections import defaultdict
 from abc import ABC, abstractmethod
 
-BUFFER_TIME = .005
-time_limit = .150 - BUFFER_TIME
+buffer = .01
+max_time = .150 - buffer
 
 
-class CustomPlayer(DataPlayer):
+class CustomPlayer_alpha_beta(DataPlayer):
+    """ Implement your own agent to play knight's Isolation
+
+    The get_action() method is the only required method for this project.
+    You can modify the interface for get_action by adding named parameters
+    with default values, but the function MUST remain compatible with the
+    default interface.
+
+    **********************************************************************
+    NOTES:
+    - The test cases will NOT be run on a machine with GPU access, nor be
+      suitable for using any other machine learning techniques.
+
+    - You can pass state forward to your agent on the next turn by assigning
+      any pickleable object to the self.context attribute.
+    **********************************************************************
+    """
+    def get_action(self, state):
+        """ Employ an adversarial search technique to choose an action
+        available in the current state calls self.queue.put(ACTION) at least
+
+        This method must call self.queue.put(ACTION) at least once, and may
+        call it as many times as you want; the caller will be responsible
+        for cutting off the function after the search time limit has expired.
+
+        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+
+        **********************************************************************
+        NOTE: 
+        - The caller is responsible for cutting off search, so calling
+          get_action() from your own code will create an infinite loop!
+          Refer to (and use!) the Isolation.play() function to run games.
+        **********************************************************************
+        """
+        # TODO: Replace the example implementation below with your own search
+        #       method by combining techniques from lecture
+        #
+        # EXAMPLE: choose a random move without any search--this function MUST
+        #          call self.queue.put(ACTION) at least once before time expires
+        #          (the timer is automatically managed for you)
+        
+        import random
+        if state.ply_count<2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            for d in range (1, 100):
+                self.queue.put(self.alpha_beta_search(state, d))
+
+    def min_value(self, gameState, alpha, beta, depth):
+        """ Return the value for a win (+1) if the game is over,
+        otherwise return the minimum value over all legal child
+        nodes.
+        """
+        if gameState.terminal_test():
+            return gameState.utility(0)
+
+        if depth<=0:
+            return self.score(gameState)
+    
+        v = float("inf")
+        for a in gameState.actions():
+            v = min(v, self.max_value(gameState.result(a), alpha, beta, depth-1))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    def alpha_beta_search(self, gameState, depth):
+        """ Return the move along a branch of the game tree that
+        has the best possible value.  A move is a pair of coordinates
+        in (column, row) order corresponding to a legal move for
+        the searching player.
+        
+        You can ignore the special case of calling this function
+        from a terminal state.
+        """
+        alpha = float("-inf")
+        beta = float("inf")
+        best_score = float("-inf")
+        best_move = None
+        
+        for a in gameState.actions():
+            v = self.min_value(gameState.result(a), alpha, beta, depth-1)
+            alpha = max(alpha, v)
+            if v > best_score:
+                best_score = v
+                best_move = a
+        return best_move
+
+
+
+    # TODO: modify the function signature to accept an alpha and beta parameter
+    def max_value(self, gameState, alpha, beta, depth):
+        """ Return the value for a loss (-1) if the game is over,
+        otherwise return the maximum value over all legal child
+        nodes.
+        """
+        if gameState.terminal_test():
+            return gameState.utility(0)
+
+        if depth<=0:
+            return self.score(gameState)
+    
+        v = float("-inf")
+        for a in gameState.actions():
+            v = max(v, self.min_value(gameState.result(a), alpha, beta, depth-1))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+          
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
+
+
+
+
+class CustomPlayer_mcts(DataPlayer):
     """ Implement your own agent to play knight's Isolation
 
     The get_action() method is the only required method for this project.
@@ -67,14 +189,14 @@ class CustomPlayer(DataPlayer):
 
         start_time = time.time()
 
-        while time.time() - start_time < time_limit:           
+        while time.time() - start_time < max_time:           
             v = self.node_selection()
             player_won = v.simulation()
             v.backpropagate(player_won)
         if self.root.children:
             return self.root.best_child(c_parameter = 0.5).whatactionwasperformedfcs
         else:
-            return random.choice(self.root.state.actions)
+            return random.choice(self.root.state.actions())
         
         
     def node_selection(self):
@@ -162,3 +284,5 @@ class monteCarloNode():
             self._result[1] += 0.5
         if self.parent:
             self.parent.backpropagate(player_won)
+
+CustomPlayer = CustomPlayer_mcts
